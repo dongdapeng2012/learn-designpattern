81b4657748b27454060e9485ec48764d723404a1
Dapeng Dong <dongdapeng2012@sina.com>
2018-01-03 01:35:06 +0800

    remove unused imports

--- src/main/java/com/student/util/MathMatrixUtil.java
+++ src/main/java/com/student/util/MathMatrixUtil.java
@@ -2,9 +2,6 @@ package com.student.util;
 
 import org.springframework.stereotype.Service;
 
-import com.student.constant.MathUtilException;
-import com.student.constant.MathUtilExceptionEnum;
-
 @Service("MathMatrixUtil")
 public class MathMatrixUtil {
 	
